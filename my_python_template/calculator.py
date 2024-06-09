bece91b67b8f82bb2292374be5ea8ea766ca17be
alexander-carrels <alexander.carrels@outlook.com>
2024-06-09 18:38:47 +0200

    make pylint checks pass

--- my_python_template/calculator.py
+++ my_python_template/calculator.py
@@ -1,2 +1,6 @@
-def my_sum(a: float, b: float):
-    return a + b
+"""An exemplary python module."""
+
+
+def my_sum(input_one: float, input_two: float):
+    """Return the sum of two inputs."""
+    return input_one + input_two
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,9 +1,8 @@
+"""Testscases for the calculator module go here."""
+
 from my_python_template.calculator import my_sum
 
 
 def test_my_sum_one_plus_one_is_two():
+    """An exemplary testcase for my_sum()"""
     assert my_sum(1, 1) == 2
-
-
-def test_my_sum_one_plus_one_is_not_three():
-    assert my_sum(1, 1) != 3
